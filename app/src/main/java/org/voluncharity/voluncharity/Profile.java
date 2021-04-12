9013d865ad25d802c1fe9d5688edefe0411bd974
Jacinto Molina <43961698+chinto40@users.noreply.github.com>
2021-04-12 17:46:14 -0500

    Changes to the Profile Page. Made simple edit button to change Volunteer's Name.

--- app/src/main/java/org/voluncharity/voluncharity/Profile.java
+++ app/src/main/java/org/voluncharity/voluncharity/Profile.java
@@ -5,9 +5,17 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static org.voluncharity.voluncharity.HomePageActivity.directActivity;
 
 public class Profile extends AppCompatActivity {
     DrawerLayout drawerLayout;
+    TextView NameId;
+    Button editProButtonId;
+    EditText editNameId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,9 @@ public class Profile extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawer_layout);
 
+        editNameId = (EditText) findViewById(R.id.editNameId);
+        NameId= (TextView) findViewById(R.id.NameId);
+        editProButtonId = (Button) findViewById(R.id.editProButtonId);
 
     }
     public void MenuClick(View view){
@@ -27,15 +38,68 @@ public class Profile extends AppCompatActivity {
 
     }
 
+    public void EditProButtonOnClick(View view){
+        System.out.println("In Next Event Button");
+        if(this.editProButtonId.getText().equals("Edit")){ //toggle to save
+                this.editProButtonId.setText("Save");
+                //this.editNameId.setClickable(true);
+                this.editNameId.setEnabled(true);
+                this.editNameId.setText("");
+                this.editNameId.setVisibility(View.VISIBLE);
+
+                this.NameId.setEnabled(false);
+                this.NameId.setVisibility(View.INVISIBLE);
+        }else if(this.editProButtonId.getText().equals("Save")){
+                this.editProButtonId.setText("Edit");
+                //this.editNameId.setClickable(false);
+                this.NameId.setText(this.editNameId.getText());
+                this.NameId.setEnabled(true);
+
+                this.NameId.setVisibility(View.VISIBLE);
+                this.editNameId.setEnabled(false);
+                this.editNameId.setVisibility(View.INVISIBLE);
+        }
+    }
+
+
+    public void NextEventOnClick(View view){
+        System.out.println("In Next Event Button");
+    }
+
+    public void EventHistoryOnClick(View view){
+        System.out.println("In Event History Button");
+    }
+    public void FavOrgOnClick(View view){
+        System.out.println("In Fav Org. Button");
+    }
+    public void DonationHistoryOnClick(View view){
+        System.out.println("In Donation History Button");
+    }
+
+    public void StoreOnClick(View view){
+        System.out.println("Inside Store Click Button");
+    }
+
+    public void RewardsOnClick(View view){
+        System.out.println("Inside Rewards Click Button");
+    }
+
+    public void LogoutOnClick(View view){
+        System.out.println("Inside Log out Click Button");
+        directActivity(this,LoginFragment.class);
+    }
+
+
+
     public void ClickEvent(View view){
-        HomePageActivity.directActivity(this,HomePageActivity.class);
+        directActivity(this,HomePageActivity.class);
 
     }
     public void ClickProfile(View view){
         HomePageActivity.closeMenu(drawerLayout);
     }
     public void ClickSettings(View view){
-       HomePageActivity.directActivity(this,Settings.class);
+       directActivity(this,Settings.class);
     }
 
     public void ClickLogout(View view){
