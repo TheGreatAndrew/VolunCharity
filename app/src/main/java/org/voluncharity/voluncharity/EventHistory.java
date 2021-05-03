a90ed4a037bca896a435ab8150ffc4d26f56f823
Xi Tang <2215007619@qq.com>
2021-05-03 10:24:43 +0800

    create eventHistory page

---
+++ app/src/main/java/org/voluncharity/voluncharity/EventHistory.java
@@ -0,0 +1,14 @@
+package org.voluncharity.voluncharity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_history);
+    }
+}
